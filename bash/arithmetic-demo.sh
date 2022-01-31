b0f3c20fae6a18e7860710f77789c6a54e0ae518
nidahanfi <nidahanfi2002@gmail.com>
2022-01-31 08:34:51 -0800

    Created helloworld script and took screenshots

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -6,15 +6,22 @@
 # Task 2: Change the output to only show:
 #    the sum of the 3 numbers with a label
 #    the product of the 3 numbers with a label
+# 
+echo ""
+echo "We are going to add and multiply three numbers."
+echo ""
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+read -p "Enter the third number: " num3
+echo ""
+echo "The first number is $num1"
+echo "The second number is $num2"
+echo "The third number is $num3"
+echo ""
 
-firstnum=5
-secondnum=2
-sum=$((firstnum + secondnum))
-dividend=$((firstnum / secondnum))
-fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
-
-cat <<EOF
-$firstnum plus $secondnum is $sum
-$firstnum divided by $secondnum is $dividend
-  - More precisely, it is $fpdividend
-EOF
+sum=$(($num1 + $num2 + $num3))
+mul_total=$(($num1 * $num2 * $num3))
+echo "The sum of three numbers is: $sum, the product of three numbers is: $mul_total"
+echo ""
+echo $sum
+echo $mul_total
